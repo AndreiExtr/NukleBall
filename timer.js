cce39508d3b52985f6c9fdaaa9066220a8f5c744
andreiextr <andreikropinov17121988@gmail.com>
2023-08-26 20:58:30 +0300

    new

--- timer.js
+++ timer.js
@@ -18,17 +18,17 @@ document.addEventListener('DOMContentLoaded', function () {
       const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
       const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
       const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-      $days.textContent = days < 10 ? '0' + days : days;
+
       $hours.textContent = hours < 10 ? '0' + hours : hours;
       $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
       $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
-      $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+
       $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
       $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
       $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
     }
     // получаем элементы, содержащие компоненты даты
-    const $days = document.querySelector('.timer__days');
+
     const $hours = document.querySelector('.timer__hours');
     const $minutes = document.querySelector('.timer__minutes');
     const $seconds = document.querySelector('.timer__seconds');
