0ef28d31962d2c14efd57d1280f1282e8248da0b
andreiextr <andreikropinov17121988@gmail.com>
2023-08-31 21:09:15 +0300

    cards

--- card.js
+++ card.js
@@ -2,8 +2,37 @@
 const cards = document.querySelectorAll('.card_gamer'); // Выбираем все элементы с классом 'card'
 
 cards.forEach(card => {
-    card.addEventListener('click', function() {
+    cards.addEventListener('click', function() {
         // Перейти на второй сайт
-        window.location.href = 'gamer.html'; // Замените на URL второго сайта
+        window.location.href = 'page8.html'; // Замените на URL второго сайта
     });
 });
+
+
+// const card = document.getElementById('card');
+// const pageDiv = document.getElementById('page8');
+
+// card.addEventListener('click', function() {
+//     // Загрузка контента другой страницы
+//     loadContent('second_page.html');
+// });
+
+// function loadContent(url) {
+//     fetch(url)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.text();
+//         })
+//         .then(data => {
+//             pageDiv.innerHTML = data;
+//         })
+//         .catch(error => {
+//             console.error('There was a problem with the fetch operation:', error);
+//         });
+// }
+
+// function goToPage(page8) {
+//     window.location.href = page8;
+// }
