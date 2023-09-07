3fe7a14ce58791793b4c6d800a5a9e354210549e
andreiextr <andreikropinov17121988@gmail.com>
2023-09-07 19:31:33 +0300

    scroll end

--- scroll.js
+++ scroll.js
@@ -1,42 +1,72 @@
-// const scrollLeftButton = document.getElementById('scroll-left');
-// const scrollRightButton = document.getElementById('scroll-right');
-// const cardsContainer = document.querySelector('.slider');
 
-// scrollLeftButton.addEventListener('click', () => {
-//   cardsContainer.scrollBy({
-//     left: -300, // Скроллируем влево на ширину карточки
-//     behavior: 'smooth', // Плавный скроллинг
-//   });
+
+// const slider = document.querySelector('.slider');
+// const scrollLeft = document.querySelector('.scroll-left');
+// const scrollRight = document.querySelector('.scroll-right');
+
+// let currentIndex = 0;
+
+// scrollLeft.addEventListener('click', () => {
+//   if (currentIndex > 0) {
+//     currentIndex--;
+//     updateSlider();
+//   }
 // });
 
-// scrollRightButton.addEventListener('click', () => {
-//   cardsContainer.scrollBy({
-//     left: 300, // Скроллируем вправо на ширину карточки
-//     behavior: 'smooth', // Плавный скроллинг
-//   });
+// scrollRight.addEventListener('click', () => {
+//   if (currentIndex < slider.children.length - 1) {
+//     currentIndex++;
+//     updateSlider();
+//   }
 // });
+
+// function updateSlider() {
+//   const translateX = -currentIndex * (slider.offsetWidth + 5); // Учтите отступ между карточками
+//   slider.style.transform = `translateX(${translateX}px)`;
+// }
+
 
 const slider = document.querySelector('.slider');
 const scrollLeft = document.querySelector('.scroll-left');
 const scrollRight = document.querySelector('.scroll-right');
+const slids = document.querySelectorAll('.slid');
+const slidWidth = slids[0].offsetWidth + 20; // Учтите отступ между карточками
+const animationDuration = 200; // Длительность анимации в миллисекундах
 
 let currentIndex = 0;
+let isAnimating = false;
 
 scrollLeft.addEventListener('click', () => {
-  if (currentIndex > 0) {
+  if (currentIndex > 0 && !isAnimating) {
     currentIndex--;
-    updateSlider();
+    animateSlider();
   }
 });
 
 scrollRight.addEventListener('click', () => {
-  if (currentIndex < slider.children.length - 1) {
+  if (currentIndex < slids.length - 1 && !isAnimating) {
     currentIndex++;
-    updateSlider();
+    animateSlider();
   }
 });
 
-function updateSlider() {
-  const translateX = -currentIndex * (slider.offsetWidth + 20); // Учтите отступ между карточками
-  slider.style.transform = `translateX(${translateX}px)`;
+function animateSlider() {
+  const startTime = performance.now();
+  const initialX = -currentIndex * slidWidth;
+
+  function step(currentTime) {
+    const progress = (currentTime - startTime) / animationDuration;
+
+    if (progress < 1) {
+      const newX = initialX - progress * slidWidth;
+      slider.style.transform = `translateX(${newX}px)`;
+      requestAnimationFrame(step);
+    } else {
+      slider.style.transform = `translateX(${initialX}px)`;
+      isAnimating = false;
+    }
+  }
+
+  isAnimating = true;
+  requestAnimationFrame(step);
 }
