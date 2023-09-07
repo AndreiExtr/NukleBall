e907edfebcd0566ea023a0310c18c756bf9da957
andreiextr <andreikropinov17121988@gmail.com>
2023-09-07 19:12:42 +0300

    new scroll

--- scroll.js
+++ scroll.js
@@ -1,17 +1,42 @@
-const scrollLeftButton = document.getElementById('scroll-left');
-const scrollRightButton = document.getElementById('scroll-right');
-const cardsContainer = document.querySelector('.slider');
+// const scrollLeftButton = document.getElementById('scroll-left');
+// const scrollRightButton = document.getElementById('scroll-right');
+// const cardsContainer = document.querySelector('.slider');
 
-scrollLeftButton.addEventListener('click', () => {
-  cardsContainer.scrollBy({
-    left: -300, // Скроллируем влево на ширину карточки
-    behavior: 'smooth', // Плавный скроллинг
-  });
+// scrollLeftButton.addEventListener('click', () => {
+//   cardsContainer.scrollBy({
+//     left: -300, // Скроллируем влево на ширину карточки
+//     behavior: 'smooth', // Плавный скроллинг
+//   });
+// });
+
+// scrollRightButton.addEventListener('click', () => {
+//   cardsContainer.scrollBy({
+//     left: 300, // Скроллируем вправо на ширину карточки
+//     behavior: 'smooth', // Плавный скроллинг
+//   });
+// });
+
+const slider = document.querySelector('.slider');
+const scrollLeft = document.querySelector('.scroll-left');
+const scrollRight = document.querySelector('.scroll-right');
+
+let currentIndex = 0;
+
+scrollLeft.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
 });
 
-scrollRightButton.addEventListener('click', () => {
-  cardsContainer.scrollBy({
-    left: 300, // Скроллируем вправо на ширину карточки
-    behavior: 'smooth', // Плавный скроллинг
-  });
+scrollRight.addEventListener('click', () => {
+  if (currentIndex < slider.children.length - 1) {
+    currentIndex++;
+    updateSlider();
+  }
 });
+
+function updateSlider() {
+  const translateX = -currentIndex * (slider.offsetWidth + 20); // Учтите отступ между карточками
+  slider.style.transform = `translateX(${translateX}px)`;
+}
