ac73a99954c6ea64d6eb603f044cb7ef73350d45
andreiextr <andreikropinov17121988@gmail.com>
2024-04-24 09:43:41 +0300

    add hover and focuse in sidebar

---
+++ Scripts/global.js
@@ -0,0 +1,24 @@
+//ПЕРЕКЛЮЧЕНИЯ ПО ВКЛАДКАМ В SIDEBAR
+function togglePage(pageId) {
+  var pages = document.querySelectorAll('.page');
+  pages.forEach(function(page) {
+    if (page.id === pageId) {
+      page.removeAttribute('hidden');
+    } else {
+      page.setAttribute('hidden', true);
+    }
+  });
+}
+
+
+//СОСТОЯНИЯ ВКЛАДОК В SIDEBAR
+const items = document.querySelectorAll('.nav-item');
+
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      items.forEach(otherItem => {
+        otherItem.classList.remove('active');
+      });
+      item.classList.add('active');
+    });
+  });
