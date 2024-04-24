502198bf127954193ff63df3cf4c66a2605fdc5c
andreiextr <andreikropinov17121988@gmail.com>
2024-04-24 09:31:21 +0300

    fixed sidebar

--- Scripts/match_cards.js
+++ Scripts/match_cards.js
@@ -19,3 +19,45 @@ for (var i = 0; i < 6; i++) {
     }
 }
 
+//ТАЙМЕР
+document.addEventListener('DOMContentLoaded', function () {
+    const newLocal = 1;
+    // конечная дата
+    const deadline = new Date(new Date().getFullYear(), new Date().getMonth() + 1, newLocal);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+    // получаем элементы, содержащие компоненты даты
+
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
+//ТАЙМЕР
+
